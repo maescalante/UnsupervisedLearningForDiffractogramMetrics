84528d44c3e1f74d0b5a86918c6decca7e29f270
Juan Camilo <jc.castiblancor@uniandes.edu.co>
2020-06-17 17:18:46 -0500

    t sne correction

--- t_sne.py
+++ t_sne.py
@@ -6,18 +6,7 @@ mat, labels = fun.readMatrix(currentFile)
 
 label_dict = fun.create_dictionary(labels)
 
-color_dict = {
-    'Ag2Se': 'bo',
-    'AgSe': 'go',
-    'Cu2Se': 'ro',
-    'CuSe': 'co',
-    'Si': 'mo',
-    'TiN': 'yo',
-    'Ti': 'ko',
-}
-
 X = mat
 X_embedded = TSNE(n_components=2).fit_transform(X)
 
-fun.plot(label_dict, X_embedded, color_dict)
-print(labels)
+fun.plot(label_dict, X_embedded)
