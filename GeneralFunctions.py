e84cb5c733cba0231c9c5b06f1738741543f2ead
Juan Camilo <jc.castiblancor@uniandes.edu.co>
2020-06-17 16:05:53 -0500

    t sne

--- GeneralFunctions.py
+++ GeneralFunctions.py
@@ -1,5 +1,5 @@
 import csv
-
+import matplotlib.pyplot as plt
 
 #Function for importing a distance matrix from a cvs file into a python matrix
 def readMatrix (filename):
@@ -23,3 +23,40 @@ def readMatrix (filename):
                 matrix.append(fila)
 
     return matrix, labels
+
+def create_dictionary(labels):
+    """
+
+    :param labels: labels of the samples
+    :return: dictonary with index of every label
+    """
+    d = {}
+
+    i = 0
+    for l in labels:
+        l1 = l.split('.')
+        if l1[0]:
+            if l1[0] not in d:
+                d[l1[0]] = [i]
+            else:
+                lista = d[l1[0]]
+                lista.append(i)
+                d[l1[0]] = lista
+        i += 1
+
+    return d
+
+
+def plot(label_dict, X, color_dict):
+    """
+
+    :param label_dict: dictonary with index of every label
+    :param X: X after Dimentinality reduction
+    :param color_dict:
+    """
+
+    for k in label_dict:
+        for j in label_dict[k]:
+            plt.plot(X[j:, 0], X[j:, 1], color_dict[k])
+
+    plt.show()
---
+++ t_sne.py
@@ -0,0 +1,23 @@
+import GeneralFunctions as fun
+from sklearn.manifold import TSNE
+
+currentFile = 'DM  - D_PP - p_min 3 - delta 0.5 - q1 -5 - q2 -0.5.csv'
+mat, labels = fun.readMatrix(currentFile)
+
+label_dict = fun.create_dictonary(labels)
+
+color_dict = {
+    'Ag2Se': 'bo',
+    'AgSe': 'go',
+    'Cu2Se': 'ro',
+    'CuSe': 'co',
+    'Si': 'mo',
+    'TiN': 'yo',
+    'Ti': 'ko',
+}
+
+X = mat
+X_embedded = TSNE(n_components=2).fit_transform(X)
+
+fun.plot(label_dict, X_embedded, color_dict)
+print(labels)
